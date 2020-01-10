3ee831bc4a2f089f04b195606f340d13b17dd965
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2020-01-10 10:06:52 -0500

    Corrected the 03_librables.sh script
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- scripts/03_livrables.sh
+++ scripts/03_livrables.sh
@@ -3,19 +3,13 @@
 mkdir -p livrables
 
 sample_names_vdj=$(echo {1,2,3,4}_VDJ)
-sample_names_gex=$(echo c{1,2,3,4}_GEX)
+sample_names_gex=$(echo {,c}{1,2,3,4}_GEX)
 
 mkdir -p livrables
 for sample_name in ${sample_names_vdj} ${sample_names_gex}; do
-    mkdir -p livrables/${sample_names}
-    cp results/${sample_name}/outs/metrics_summary.csv livrables/${sample_name}_metrics_summary.csv
-    cp results/${sample_name}/outs/web_summary.html livrables/${sample_name}_web_summary.html
-done
-
-for sample_name in ${sample_names_vdj}; do
-    cp results/${sample_name}/outs/vloupe.vloupe livrables/${sample_name}_vloupe.vloupe
-done
-
-for sample_name in ${sample_names_gex}; do
-    cp results/${sample_name}/outs/cloupe.cloupe livrables/${sample_name}_cloupe.cloupe
+    echo ${sample_name}
+    mkdir -p livrables/${sample_name}
+    cp results/${sample_name}/outs/metrics_summary.csv livrables/${sample_name}/
+    cp results/${sample_name}/outs/web_summary.html livrables/${sample_name}/
+    cp results/${sample_name}/outs/?loupe.?loupe livrables/${sample_name}/
 done
